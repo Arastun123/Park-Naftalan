5bbccc028ffb70f91af58f6c02ae85a69efdc40d
Arastun123 <ekbr03@gmail.com>
2025-08-06 13:02:34 +0400

    Restaurant update

--- app/[locale]/restaurant/page.js
+++ app/[locale]/restaurant/page.js
@@ -1,76 +1,10 @@
-import Button from "@/components/Button/Button";
-import ContactComponent from "@/components/Contact";
-import { Clock, Download, Phone, Reserve } from "@/components/Svg";
+import Restaurant from "@/components/Restaurant/inde";
 
 import { getTranslations } from "@/lib/getTranslations";
-
-import global from "@/styles/global.module.scss";
-import styles from "@/styles/restaurant.module.scss";
 
 export default async function RestaurantPage({ params }) {
   const { locale } = await params;
   const t = await getTranslations(locale);
 
-  return (
-    <div className={styles.restaurant}>
-      <div className={styles.mainImageWrapper}>
-        <img
-          src="/Rectangle99.png"
-          alt="Park Terrace Restaurant"
-          className={styles.backgroundImage}
-        />
-        <div className={styles.mainImage}>
-          <h2>{t?.Park}</h2>
-          <p>{t?.ResTxt}</p>
-          <div className={styles.actions}>
-            <Button>
-              <Reserve /> {t?.Reserv}
-            </Button>
-            <Button>
-              <Download /> {t?.Menu}
-            </Button>
-          </div>
-        </div>
-      </div>
-      <div className={global.container}>
-        <div className={styles.about}>
-          <div className={styles.text}>
-            {" "}
-            <h3>{t?.Park}</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Commodo venenatis egestas
-              mollis id magna nisl ut. Odio faucibus tellus hac justo eu
-              faucibus.Lorem ipsum dolor sit amet consectetur. Commodo venenatis
-              egestas mollis id magna nisl ut. Odio faucibus tellus hac justo eu
-              faucibus.
-            </p>
-            <div className={styles.cards}>
-              <div className={styles.card}>
-                <Clock />
-                <p>Open hours</p>
-                <p>09:00 - 24:00</p>
-                <p>Every day</p>
-              </div>
-              <div className={styles.card}>
-                <Phone color="#333" width="30" height="30" />
-                <p>Open hours</p>
-                <p>09:00 - 24:00</p>
-                <p>Every day</p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.image}>
-            <img src="/Rectangle99.png" alt="Park Terrace Restaurant" />
-          </div>
-        </div>
-        <div className={styles.gallery}>
-          <h3>{t?.Discover}</h3>
-
-          <div className={styles.grid}>
-            
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Restaurant t={t} locale={locale} />;
 }
---
+++ components/Restaurant/inde.js
@@ -0,0 +1,90 @@
+"use client";
+import Button from "@/components/Button/Button";
+import { Clock, Download, Phone, Reserve } from "@/components/Svg";
+
+import global from "@/styles/global.module.scss";
+import styles from "@/styles/restaurant.module.scss";
+
+export default function Restaurant({ t, locale }) {
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = `/files/menu${locale}qiymet.pdf`;
+    link.download = "menu.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
+  return (
+    <div className={styles.restaurant}>
+      <div className={styles.mainImageWrapper}>
+        <img
+          src="/main.jpg"
+          alt="Park Terrace Restaurant"
+          className={styles.backgroundImage}
+        />
+        <div className={styles.mainImage}>
+          <h2>{t?.Park}</h2>
+          <p>{t?.ResTxt}</p>
+          <div className={styles.actions}>
+            <Button>
+              <Reserve /> {t?.Reserv}
+            </Button>
+            <Button onClick={handleDownload}>
+              <Download /> {t?.Menu}
+            </Button>
+          </div>
+        </div>
+      </div>
+      <div className={global.container}>
+        <div className={styles.about}>
+          <div className={styles.text}>
+            {" "}
+            <h3>{t?.Park}</h3>
+            <p>{t?.park_text.intro}</p>
+            <p>{t?.park_text.choice}</p>
+            <p>{t?.park_text.here_you_can}</p>
+            <ul>
+              <li>{t?.park_text.item_1}</li>
+              <li>{t?.park_text.item_2}</li>
+              <li>{t?.park_text.item_3}</li>
+            </ul>
+            <p>{t?.park_text.music}</p>
+            <p>{t?.park_text.end}</p>
+            <div className={styles.cards}>
+              <div className={styles.card}>
+                <Clock />
+                <p>Open hours</p>
+                <p>09:00 - 24:00</p>
+                <p>Every day</p>
+              </div>
+              <div className={styles.card}>
+                <Phone color="#333" width="30" height="30" />
+                <p>Open hours</p>
+                <p>09:00 - 24:00</p>
+                <p>Every day</p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.image}>
+            <img src="/second.jpg" alt="Park Terrace Restaurant" />
+          </div>
+        </div>
+        <div className={styles.gallery}>
+          <h3>{t?.Discover}</h3>
+
+          <div className={styles.grid}>
+            {Array.from({ length: 9 }, (_, i) => (
+              <img
+                key={i}
+                src={`/${i + 1}.jpg`}
+                alt={`Image ${i + 1}`}
+                className={styles.image}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
